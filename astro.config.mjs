722a1fc9fbc6066e28cfa19af801f77ecfa6cbf0
Julien V <vier.julien@googlemail.com>
2024-08-16 11:48:39 +0100

    Implemented and style subscribe form

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import sitemap from "@astrojs/sitemap";
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://newsletter.supply',
-  integrations: [tailwind(), sitemap()]
+  integrations: [tailwind(), sitemap()],
+  output: "server",
+  adapter: netlify()
 });
