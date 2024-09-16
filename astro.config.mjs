a2167f560ae89580cd14ff7a580a6054d5a93b9b
Julien V <vier.julien@googlemail.com>
2024-09-16 16:04:05 +0100

    test redirects 2

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,14 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 
-import netlify from "@astrojs/netlify";
-
 import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://newsletter.supply',
   integrations: [tailwind(), sitemap(), icon()],
+  trailingSlash: 'never',
   output: "server",
-  adapter: netlify(),
   build: {
     format: 'file'
   },
