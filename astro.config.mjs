1f4bc85ff0f6599a09be18c7f9d9bc196527a467
Julien V <vier.julien@googlemail.com>
2024-09-16 16:05:36 +0100

    another try netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 
+import netlify from "@astrojs/netlify";
+
 import icon from "astro-icon";
 
 // https://astro.build/config
@@ -10,6 +12,7 @@ export default defineConfig({
   integrations: [tailwind(), sitemap(), icon()],
   trailingSlash: 'never',
   output: "server",
+  adapter: netlify(),
   build: {
     format: 'file'
   },
