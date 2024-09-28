2b7dca8a8d3b897d5e0d48a3ff97dd6689f5fb46
Julien V <vier.julien@googlemail.com>
2024-09-28 16:45:39 +0100

    build project

--- .netlify/build/chunks/framer_tL_qrkcD.mjs
+++
@@ -1,9 +0,0 @@
-async function getMod() {
-						return import('./framer_DSrtTO-Y.mjs');
-					}
-					const collectedLinks = [];
-					const collectedStyles = [];
-					const collectedScripts = [];
-					const defaultMod = { __astroPropagation: true, getMod, collectedLinks, collectedStyles, collectedScripts };
-
-export { defaultMod as default };
