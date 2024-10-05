c8665623c1fa7c1959b61f4b6f71a02482a3a6cd
Julien V <vier.julien@googlemail.com>
2024-10-05 08:07:03 +0100

    dark styling and categories

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,21 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
+		colors: {
+			'zinc': {
+				'50': '#09090b',
+				'100': '#18181b',
+				'200': '#27272a',
+				'300': '#3f3f46',
+				'400': '#52525b',
+				'500': '#71717a',
+				'600': '#a1a1aa',
+				'700': '#d4d4d8',
+				'800': '#e4e4e7',
+				'900': '#f4f4f5',
+				'950': '#fafafa',
+			},
+		},
 	},
 	plugins: [],
 }
